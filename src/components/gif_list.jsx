76bf50296266ddeb36e09ad8345c1fbcac947aa0
adamzermane <adamzermane92@yahoo.com>
2022-05-24 14:05:47 +0700

    react giphy page

---
+++ src/components/gif_list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Gif from './gif';
+
+const GifList = (props) => {
+  return (
+    <div className="gif list">
+      {props.gifs.map(gif => <Gif id={gif} selectGif={props.selectGif} />)}
+    </div>
+  );
+};
+
+
+export default GifList;
